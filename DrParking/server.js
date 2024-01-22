98db1d6f0704d500ae1b1c22bf67d0e6cccf475c
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2024-01-22 16:46:00 +0200

    Delete DrParking directory

--- DrParking/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Parking managemnt system is running on on port ${port}!`))
